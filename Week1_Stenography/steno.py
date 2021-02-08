c1516f2ab9c310042ea58d2c9f296972775c33bc
Runi <Runiniclasen@hotmail.com>
2021-02-08 16:54:27 +0100

    add initial comments

--- Week1_Stenography/steno.py
+++ Week1_Stenography/steno.py
@@ -5,10 +5,10 @@ image = Image.open('7fe3c3f6-Stego.png')
 extracted = ''
 
 pixels = image.load()
-for i in range(0, image.height):
-    for j in range(0, image.width):
-        r, g, b, a = pixels[j, i]
-        extracted += bin(b)[-1]
+for i in range(0, image.height): 
+    for j in range(0, image.width): 
+        r, g, b, a = pixels[j, i] # (x, y)
+        extracted += bin(b)[-1] # extract binary
 
 chars = []
 for i in range(int(len(extracted)/8)):
