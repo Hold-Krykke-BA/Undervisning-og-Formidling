1c664549840178862ebd7145d75d06d5699681b4
Runi <Runiniclasen@hotmail.com>
2021-02-08 17:55:24 +0100

    cleanup / documentation

--- Week1_Stenography/steno.py
+++ Week1_Stenography/steno.py
@@ -1,22 +1,20 @@
 from PIL import Image
 
 image = Image.open('7fe3c3f6-Stego.png')
+pixels = image.load()
 
 extracted = ''
-
-pixels = image.load()
 for i in range(0, image.height): 
     for j in range(0, image.width): 
         r, g, b, a = pixels[j, i] # (x, y)
-        extracted += bin(b)[-1] # extract binary
+        extracted += bin(b)[-1] # extract binary (LSB?)
 
-chars = []
-for i in range(int(len(extracted)/8)):
-    byte = extracted[i*8:(i+1)*8][::-1]
+secret = ''
+for i in range(int(len(extracted)/8)): # for every 8th entry...
+    byte = extracted[i*8:(i+1)*8][::-1] # get 8-bit (byte) combination at index + 8. Reverse for actual order. (LSB?)
     if byte == '00000000':
         break
-    chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
+    secret += chr(int(byte, 2)) #convert byte to integer from base-2 (binary), then finally to char for string representation.
 
-secret = ''.join(chars)
-print(secret)
+print(secret) 
 # Congratulations, this is the secret message of the UFO class! (no, not 42)
