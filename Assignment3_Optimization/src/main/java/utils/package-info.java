a2c94333b541bc73c8cbc54836cf912708441dfb
Runi <Runiniclasen@hotmail.com>
2021-04-12 16:57:42 +0200

    add utilities

---
+++ Assignment3_Optimization/src/main/java/utils/package-info.java
@@ -0,0 +1,7 @@
+package utils;
+
+/**
+ * These files were found here: https://datsoftlyngby.github.io/soft2021spring/resources/ee799a67-SestoftMicrobenchmarking.pdf
+ * <p>
+ * Direct .zip download: http://www.itu.dk/people/sestoft/javaprecisely/benchmarks-java.zip
+ */
